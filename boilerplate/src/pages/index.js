5c89b4aa7e2db789284cc85e89f122f1373803e5
coderg <ghs_source@sina.com>
2022-11-14 17:02:42 +0800

    chore: 前5节，第六节start

--- boilerplate/src/pages/index.js
+++ boilerplate/src/pages/index.js
@@ -1,3 +1,23 @@
 
+import { Link, useSearchParams, createSearchParams, useLocation } from "umi";
 
-export default () => <div>Hello Work</div>
+export default () => {
+
+  const [SearchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  const a = SearchParams.get('a')
+  const b = SearchParams.get('b')
+
+  return (
+    <div>Hello Work
+      <p><Link to="/user">Go to user page</Link></p>
+      <div>
+        <p>SearchParams ----- a:{a}; b:{b}</p>
+        <p>State --- {JSON.stringify(location?.state)}</p>
+        <button
+          onClick={() => {setSearchParams(createSearchParams({a: 123, b: 456}))}}
+        >Change SearchParams</button>
+      </div>
+    </div>
+  )
+}
