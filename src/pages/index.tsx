b5d983ee7f5965b0eb31f0462ec5bd005449eee9
coderg <ghs_source@sina.com>
2022-11-15 18:44:35 +0800

    chore: umi中资产，引入图片并在typings.d.ts声明配置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 
 import { Link, useSearchParams, createSearchParams, useLocation } from "umi";
 import React from "react";
+import logoImg from "@/assets/logo.png"
 
 export default () => {
 
@@ -12,6 +13,7 @@ export default () => {
   return (
     <div>Hello Work
       <p><Link to="/user">Go to user page</Link></p>
+      <img src={logoImg} alt="image" />
       <div>
         <p>SearchParams ----- a:{a}; b:{b}</p>
         <p>State --- {JSON.stringify(location?.state)}</p>
